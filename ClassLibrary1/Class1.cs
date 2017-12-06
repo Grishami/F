7247caf6854e81d93bbf45855f9ec27efe216ded
User <User@K3-512-20>
2017-12-06 21:16:18 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Dofac (int n)
+        {
+            int f = 1;
+
+            for (int nn = n; nn >= 1; nn--)
+                f = f * nn;
+
+            return f;
+            
+
+        }
+    }
+}
